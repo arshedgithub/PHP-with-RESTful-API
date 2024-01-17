070192ca5562b85c0c302aadbeee996afe2a87e0
arshedgithub <arshedahmedmail@gmail.com>
2024-01-17 06:27:06 +0530

    initializing with signup form

---
+++ backend/signup.php
@@ -0,0 +1,14 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_REQUEST['name']);
+    $pwd = htmlspecialchars($_REQUEST['pwd']);
+
+    if (empty($name) || empty($pwd)) {
+        echo "field empty";
+    } else {
+        // echo $name;
+    }
+}
+
+?>
